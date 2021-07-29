38bd837e1b21c1537236b836ee87dbb8d470c295
jamessteele <jamessteele@IT-ES-002AM.local>
2021-07-29 17:10:20 -0400

    fix health check

--- lib/agile/web/router.ex
+++ lib/agile/web/router.ex
@@ -10,7 +10,7 @@ defmodule Web.Router do
   # Executes matched code (from the match plug)
   plug(:dispatch)
 
-  get "/ping" do
-    send_resp(conn, 200, "pong")
+  get _ do
+    send_resp(conn, 200, "hi")
   end
 end
